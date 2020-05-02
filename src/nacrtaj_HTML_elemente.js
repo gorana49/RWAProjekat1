0bcea7060eaeb4b2d945bd37ef5fadb64a4fe946
gorana49 <gorana@elfak.rs>
2020-05-02 20:29:42 +0200

    inicijalizacija dev-a2

--- src/nacrtaj_HTML_elemente.js
+++ src/nacrtaj_HTML_elemente.js
@@ -1,14 +1,15 @@
 export function nacrtajPocetnuStranu(roditelj)
 {
     const kontejner = napraviElement(roditelj, "div", "kontejner");
-    const naslov= napraviElement(roditelj, "div", naslov);
+    const naslov= napraviElement(roditelj, "div", "naslov");
     const dugmiciSlika= napraviElement(roditelj,"div", "dugmiciSlika");
 
     const dugmici= nacrtajDugmice(dugmiciSlika);
     const slika= napraviElement(dugmiciSlika,"div","slika");
-
-
-
+    
+     let image = napraviElement(slika, "img","image");
+     image.src= 'C:\Users\radov\RWAProjekat1\RWAProjekat1\photos\slika1.jpg';
+     
 }
 
 export function napraviElement(roditelj, tip, imeKlase)
@@ -21,7 +22,9 @@ export function napraviElement(roditelj, tip, imeKlase)
 
 export function nacrtajDugmice(roditelj)
 {
-        
-
+    let dugmeDiv=napraviElement(roditelj, "div", "dugmeDiv");
+    let dugme1 = napraviElement(roditelj, "button", "dugme");
+    let dugme2 = napraviElement(roditelj, "button", "dugme");
+    let dugme3 = napraviElement(roditelj, "button", "dugme");
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
